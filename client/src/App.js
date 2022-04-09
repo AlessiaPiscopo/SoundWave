1fa43831db12f672ec94778f672faf27b5034f1b
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-04-09 11:45:06 -0400

    create dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 
-import Home from "./pages/Home";
+import Homepage from "./pages/Homepage";
 import SignUp from "./pages/SignUp";
 import Login2 from "./pages/Login2";
 import Profile from "./pages/Profile";
@@ -15,7 +15,7 @@ const App = () => {
         <GlobalStyles />
 
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Homepage />} />
 
           <Route exact path="/signup" element={<SignUp />} />
           <Route exact path="/login" element={<Login2 />} />
