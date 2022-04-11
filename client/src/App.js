236b46647635a41ceacffbe3ce99589ab1a7332c
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-04-11 05:14:44 -0400

    Move data from Dropdown.js up to App.js
    Pass data down to <Dropdown /> as props

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 
 import Homepage from "./pages/Homepage";
-import Dashboard from "./pages/Dashboard";
+// import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Artist from "./pages/Artist";
 import FanNavBar from "./components/NavBar/FanNavBar";
 import ArtistDashboard from "./pages/ArtistDashboard";
+import Dropdown from "./components/Dropdown";
 
 const App = () => {
+  const data = [
+    { name: "A", value: 1 },
+    { name: "B", value: 2 },
+    { name: "C", value: 3 },
+  ];
+
   return (
     <>
       <BrowserRouter>
@@ -18,7 +25,8 @@ const App = () => {
         <FanNavBar />
         <Routes>
           <Route exact path="/" element={<Homepage />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Dropdown data={data} />
+          {/* <Route exact path="/dashboard" element={<Dashboard />} /> */}
 
           <Route exact path="/signup" element={<SignUp />} />
           <Route exact path="/login" element={<Login />} />
--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,18 +1,14 @@
-const Dropdown = () => {
-  const testData = [
-    { name: "A", value: 1 },
-    { name: "B", value: 2 },
-    { name: "C", value: 3 },
-  ];
-
+const Dropdown = ({ data }) => {
   return (
     <>
       <div>
         <select name="" id="">
-          {testData.map((item, index) => {
-            <option key={index} value={testData.value}>
-              {item.name}
-            </option>;
+          {data.map((item, index) => {
+            return (
+              <option key={index} value={data.value}>
+                {item.name}
+              </option>
+            );
           })}
         </select>
       </div>
