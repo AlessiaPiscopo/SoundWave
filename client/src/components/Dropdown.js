9d1824ef0f0ab65bdde6bfe65ba037b6aa9c14e4
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-04-11 05:02:05 -0400

    Update Dropdown.js
    Create testData to test our map method.

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,5 +1,23 @@
 const Dropdown = () => {
-    return ( <><div>dropdown</div></> );
-}
- 
+  const testData = [
+    { name: "A", value: 1 },
+    { name: "B", value: 2 },
+    { name: "C", value: 3 },
+  ];
+
+  return (
+    <>
+      <div>
+        <select name="" id="">
+          {testData.map((item, index) => {
+            <option key={index} value={testData.value}>
+              {item.name}
+            </option>;
+          })}
+        </select>
+      </div>
+    </>
+  );
+};
+
 export default Dropdown;
